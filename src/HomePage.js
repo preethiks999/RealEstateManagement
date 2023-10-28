e8106075449adda88f806887c441baae1e73f66b
Hitesh Sankhat <hiteshpankaj.sankhat@cimpress.com>
2023-10-28 23:53:25 +0530

    updated project card UI

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,9 +3,17 @@ import Project from "./Project";
 import React from "react";
 
 const HomePage = () => {
+  const data = [
+    ...sampleData,
+    ...sampleData,
+    ...sampleData,
+    ...sampleData,
+    ...sampleData,
+    ...sampleData,
+  ];
   return (
-    <div className="">
-      {sampleData.map((p, index) => (
+    <div className="grid grid-cols-auto-fit gap-x-3 gap-y-2 my-6 mx-3">
+      {data.map((p, index) => (
         <Project key={index} data={p}></Project>
       ))}
     </div>
