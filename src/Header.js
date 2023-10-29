48d7e4fc6708bdcc283d53a1bc84f956d9334504
bhavya yadati <preethiks999@gmail.com>
2023-10-30 02:48:18 +0530

    css changes

--- src/Header.js
+++ src/Header.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [btnName, setBtnName] = useState("Login");  
   return (
-    <div className="flex justify-between bg-blue-400 shadow-lg">
+    <div className="flex justify-between bg-indigo-900 shadow-lg">
       <div className="logo-container w-[120px]">
         <img  src="https://housing-images.n7net.in/afe3f526/5b76eee6ef7c5a2349b28c65c23f2571/v2/medium.jpg"></img>
       </div>
       <div className="flex">
         <ul className="flex p-4 m-4">
         
-          <li className="px-4"><Link to="/">Home</Link></li>
-          <li className="px-4"> <Link to="/about">  About </Link></li>
-          <li className="px-4"> <Link to="/contact"> Contact </Link></li>          
-          <li className="px-4">
+          <li className="px-4 text-white"><Link to="/">Home</Link></li>
+          <li className="px-4  text-white"> <Link to="/about">  About </Link></li>
+          <li className="px-4  text-white"> <Link to="/contact"> Contact </Link></li>          
+          <li className="px-4  text-white">
             <button
               onClick={() => {
                 btnName === "Login"
