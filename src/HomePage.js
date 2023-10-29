c7b09b1cd860069cd91a35f48e565073faf24314
bhavya yadati <preethiks999@gmail.com>
2023-10-30 01:13:56 +0530

    price range slider changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,13 @@
- import { sampleData } from "./data";
+import { sampleData } from "./data";
 import Project from "./Project";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
 
 const HomePage = () => {
+  // const [minPrice, setMinPrice] = useState(0);
+  // const [maxPrice, setMaxPrice] = useState(0);
 
+  
   const data = [
     ...sampleData,
     ...sampleData,
@@ -13,39 +16,73 @@ const HomePage = () => {
     ...sampleData,
     ...sampleData,
   ];
+  const inventories = data.map((x) => x.inventories);
+  const listOfInventories = [].concat(...inventories);
 
-  const [filtered,setFilteredProjects]  = useState(data);
-  
-  const inventories = data.map(x=>x.inventories);  
-  const flats = [].concat(...inventories).map(x=>{return {name:x.inventoryName,id:x.inventoryId}});
-  const filteredFlats = [...new Set(flats.map(item => item.name))];
-  const result = filteredFlats.map((value, index) => ({
+  const allPrices = listOfInventories.map((item) => item.price / 100000);
+  const minPrice =  Math.min(...allPrices);
+  const maxPrice = Math.max(...allPrices);
+
+  // useEffect(() => {
+  //   console.log(data.map((x) => x.inventories));
+  //   const allPrices = listOfInventories.map((item) => item.price / 100000);
+  //   setMaxPrice(Math.max(...allPrices));
+  //   setMinPrice(Math.min(...allPrices));
+  // }, []);
+
+  const [filtered, setFilteredProjects] = useState(data);
+
+  const flats = listOfInventories.map((x) => {
+    return { name: x.inventoryName, id: x.inventoryId };
+  });
+  const filteredFlats = [...new Set(flats.map((item) => item.name))];
+  const filteredFlatsDropdownArray = filteredFlats.map((value, index) => ({
     index: index + 2,
     value,
   }));
-  
-  searchHandler = (value)=>{
-    setFilteredProjects(data.filter(x=>x.projectName.toLowerCase().includes(value.toLowerCase())));
-  }
 
-  searchFlatsHandler = (value)=>{
-    if(value>1)
-    {
-      const name = result.filter(x=>x.index == value)[0].value;
-      setFilteredProjects(data.filter(x=>x.inventories.filter(i=>i.inventoryName.includes(name)).length>0));
-    } 
-    else
-    {
+  searchHandler = (value) => {
+    setFilteredProjects(
+      data.filter((x) =>
+        x.projectName.toLowerCase().includes(value.toLowerCase())
+      )
+    );
+  };
+
+  searchFlatsHandler = (value) => {
+    if (value > 1) {
+      const name = result.filter((x) => x.index == value)[0].value;
+      setFilteredProjects(
+        data.filter(
+          (x) =>
+            x.inventories.filter((i) => i.inventoryName.includes(name)).length >
+            0
+        )
+      );
+    } else {
       setFilteredProjects(data);
-    }  
+    }
+  };
+
+  searchPriceHandler = (event)=>{
+     console.log(event)
+     const min = event[0];
+     const max = event[1];
+     setFilteredProjects(data.filter(x=>x.inventories.filter(i=>i.price/100000 >= min && i.price/100000 <= max).length > 0))
   }
 
   return (
     <>
-      <Search eventHandler={searchHandler} flats = {result} flatsEventHandler = {searchFlatsHandler} ></Search>
+      <Search
+        minMaxRange = {[minPrice, maxPrice]}
+        eventHandler={searchHandler}
+        flats={filteredFlatsDropdownArray}
+        flatsEventHandler={searchFlatsHandler}
+        filterPriceHandler = {searchPriceHandler}
+      ></Search>
       <div className="grid grid-cols-auto-fit gap-x-12 gap-y-4 my-6 mx-3">
         {filtered.map((p, index) => (
-          <Project key={index} data={p}></Project> 
+          <Project key={index} data={p}></Project>
         ))}
       </div>
     </>
---
+++ src/RangeSlider.js
@@ -0,0 +1,33 @@
+import React from 'react'; 
+import Typography from '@material-ui/core/Typography'; 
+import Slider from '@material-ui/core/Slider'; 
+  
+const RangeSlider = (props) => { 
+
+  // Our States 
+  const [value, setValue] =  React.useState(props.minMaxRange);   
+  console.log(value)
+  // Changing State when volume increases/decreases 
+  const rangeSelector = (event, newValue) => {     
+    setValue(newValue);    
+    props.filterPriceHandler(newValue) 
+  };  
+  
+  return ( 
+    <div className='my-2'>       
+      <Typography id="range-slider" gutterBottom> 
+        Select Price Range: 
+      </Typography> 
+      <Slider 
+        value={value} 
+        min={props.minMaxRange[0]}
+        max={props.minMaxRange[1]}
+        onChange={rangeSelector} 
+        valueLabelDisplay="auto"
+      /> 
+      Your range of Price is between {value[0]} L and {value[1]} L 
+    </div> 
+  ); 
+} 
+  
+export default RangeSlider;
